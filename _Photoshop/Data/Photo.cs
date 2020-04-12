dd78c7c1afe98c50c3a1bac3f07c5561ead49b37
Tointer <Tointer@yandex.ru>
2020-04-13 02:28:39 +0300

    new data representation

--- _Photoshop/Data/Photo.cs
+++ _Photoshop/Data/Photo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 
 namespace MyPhotoshop
 {
@@ -6,7 +7,40 @@ namespace MyPhotoshop
 	{
 		public int width;
 		public int height;
-		public double[,,] data;
+		public Pixel[,] data;
 	}
+
+    public struct Pixel
+    {
+        public double R;
+        public double G;
+        public double B;
+
+        public Pixel(double r, double g, double b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+
+        public Pixel WithRed(double red)
+        {
+            return new Pixel(red, G, B);
+        }
+        public Pixel WithGreen(double green)
+        {
+            return new Pixel(R, green, B);
+        }
+        public Pixel WithBlue(double blue)
+        {
+            return new Pixel(R, G, blue);
+        }
+
+        public static Pixel operator*(Pixel pixel, double modifier)
+        {
+            return new Pixel(pixel.R*modifier, pixel.G*modifier, pixel.B*modifier);
+        }
+    }
+
 }
 
